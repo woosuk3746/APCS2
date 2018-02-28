6bb5f9c2024417d329ff01468ff7d22595e16eec
Woosuk Lee <wlee10@stuy.edu>
2018-02-28 13:40:55 -0500

    hw10 xd

--- hw10/QueenBoard.java
+++ hw10/QueenBoard.java
@@ -2,6 +2,12 @@
  * class QueenBoard
  * Generates solutions for N-Queens problem.
  */
+/*
+ Woosuk Lee
+ APCS2 pd8
+ HW10 --  [Freddie Mercury, Brian May, Roger Taylor, John Deacon] x n
+ 2018-02-28
+*/
 
 public class QueenBoard
 {
@@ -23,7 +29,18 @@ public class QueenBoard
    */
   public boolean solve()
   {
-    return false;
+      int r = 1;
+      int c = 0;
+      while(r < _board.length && c < _board.length){
+          addQueen(r,c);
+          r+=2;
+          c+=1;
+      }
+      for(int i = 0; i < _board.length; i++){
+          solveH(i);
+      }
+      
+      return false;
   }
 
   
@@ -32,6 +49,12 @@ public class QueenBoard
    */
   private boolean solveH( int col ) 
   {
+    
+    for (int i = 0; i < _board.length; i++){
+	   if (_board[i][col] == 0)
+           return addQueen(i,col);
+	    
+	}
     return false;
   }
 
@@ -43,6 +66,20 @@ public class QueenBoard
         all negs and 0's replaced with underscore
         all 1's replaced with 'Q'
     */
+    String retStr = "";
+	for( int r = 0; r < _board.length; r++ ) {
+	    for( int c = 0; c < _board[0].length; c++ ) {
+		if (_board[r][c] <= 0){
+		    ans += "_";
+		}
+		if (_board[r][c] == 1){
+		    ans += "Q";
+		}
+		ans += "\t";
+	    }
+	    ans += "\n";
+	}
+	System.out.println(retStr);
   }
 
 
@@ -51,19 +88,23 @@ public class QueenBoard
 
   /***
    * <General description>
-   * precondition: 
-   * postcondition: The queen is placed, and all the unacceptable queen placing positions are marked 
+   * precondition: A valid QueenBoard with at least one spot == 0
+   * postcondition: All queen's potential target marked
    */
   private boolean addQueen(int row, int col){
     if(_board[row][col] != 0){
-	    return false;
+	    return false; //cannot place queen because a queen is there or within the target
     }
-    _board[row][col] = 1;
-    int offset = 1;
+    _board[row][col] = 1; //place the queen
+      
+    /***
+    Everything hereafter is for marking the queen targets
+    ***/
+    int offset = 1; //counter to keep track of the row/column for marking the target
     while(col+offset < _board[row].length){
-	    _board[row][col+offset]--;
+	    _board[row][col+offset]--; //mark off the rows and columns
 	    if(row - offset >= 0){
-        _board[row-offset][col+offset]--;
+        _board[row-offset][col+offset]--; //mark off the diagonals (only the lower-right)
 	    }
 	    if(row + offset < _board.length){
         _board[row+offset][col+offset]--;
@@ -76,16 +117,17 @@ public class QueenBoard
 
   /***
    * <General description>
-   * precondition:
-   * postcondition: The queen is removed, and all the unacceptable queen placing marks are removed. 
+   * precondition: A valid QueenBoard with at least one queen == 1
+   * postcondition: queen potential target removed
    */
   private boolean removeQueen(int row, int col){
     if ( _board[row][col] != 1 ) {
-	    return false;
+	    return false; //if there's nothing, just stop the code
     }
-    _board[row][col] = 0;
-    int offset = 1;
+    _board[row][col] = 0; //remove it (make it 1)
+    int offset = 1; 
 
+    // same mechanism as addQueen, but just increment instead of decrement
     while( col+offset < _board[row].length ) {
 	    _board[row][col+offset]++;
 	    if( row - offset >= 0 ) {
@@ -102,10 +144,10 @@ public class QueenBoard
 
   /***
    * <General description>
-   * precondition: 
-   * postcondition: 
+   * precondition: a valid QueenBoard given
+   * postcondition: print the state of the board
    */
-  public String  toString() 
+  public String toString() 
   {
     String ans = "";
     for( int r = 0; r < _board.length; r++ ) {
