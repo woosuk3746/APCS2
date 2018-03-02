b2162ddebaf4355d28332c822d0e66bb93d16e74
Woosuk Lee <wlee10@stuy.edu>
2018-03-01 19:24:55 -0500

    hw12

---
+++ hw12/go.kt
@@ -0,0 +1,22 @@
+Woosuk Lee
+APCS2 pd8
+HW12 -- __+3R|\/|1|\|4|_  1|_|_|\|3$$__
+2018-3-2
+
+Q0: What do you make of the multiple class definitions in a single .java file?
+
+Q1: How are the command line arguments processed?
+
+Q2: What happens if no command line arguments are given?
+
+Q3: What is the nature/purpose of the recursion? Is it necessary for the animation?
+
+Q4: What do you expect to see when you run it?
+
+Q5: How long do you expect it to run?
+
+Q6: What does ANSI stand for? (You are permitted some intertubes searching here…). What is it good for?
+
+Q7: How did your expectations compare to your observations?
+
+Q8: How might you adapt this framework to animate a probing for a Knight’s Tour?
