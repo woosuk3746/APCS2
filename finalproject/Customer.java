be7dc434ef2def21f69ecb180cb324db4d3dd026
Woosuk Lee <wlee10@stuy.edu>
2018-05-31 10:09:48 -0400

    final project stuff

---
+++ finalproject/Customer.java
@@ -0,0 +1,36 @@
+public class Customer{
+
+    //instance variable
+    private String order;
+
+    //constructor
+    public Customer(){
+	/*
+	  order = stuff (to be done when class Restaurant is implemented
+	 */
+    }
+
+    /*    
+    //accessor method that returns the order of this current customer
+    //this will be used to print what food the customer orders in the terminal in the beginning of the game
+    e.g.)
+
+    Terminal will print:
+    WELCOME TO THE RESTAURANT
+
+    1st Customer wants: hamurger
+
+    Codified version:
+    SOP("WELCOME TO THE RESTAURANT");
+    SOP(n + "th Customer wants: " + customer0.getOrder() );
+
+    public String getOrder(){
+	return order;
+    }
+
+    //private method for setting order
+    private String setOrder(){
+    
+    }
+    */
+}
