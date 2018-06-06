52c8c388a577bd026e2fe8d3e080f0d0cc7220a8
Woosuk Lee <wlee10@stuy.edu>
2018-06-06 16:52:44 -0400

    lol

--- finalproject/Customer.java
+++ finalproject/Customer.java
@@ -1,36 +1,40 @@
 public class Customer{
 
     //instance variable
-    private String order;
+    private String order; //stores what food this customer wants to eat
 
     //constructor
+
     public Customer(){
-	/*
-	  order = stuff (to be done when class Restaurant is implemented
-	 */
+	order = null;
     }
 
-    /*    
-    //accessor method that returns the order of this current customer
-    //this will be used to print what food the customer orders in the terminal in the beginning of the game
-    e.g.)
+    public Customer(String theOrder){
+	order = theOrder; //set the order to the given parameter
+    }
 
-    Terminal will print:
-    WELCOME TO THE RESTAURANT
-
-    1st Customer wants: hamurger
-
-    Codified version:
-    SOP("WELCOME TO THE RESTAURANT");
-    SOP(n + "th Customer wants: " + customer0.getOrder() );
-
+    public void setOrder(){
+	order = null;
+	//TODO: MAKE A RANDOMIZER FOR THE ORDER
+    }
+    
+    /**********************************************************************************************************    
+     * accessor method that returns the order of this current customer
+     * this will be used to print what food the customer orders in the terminal in the beginning of the game
+     * 
+     * e.g.)
+     *
+     * Terminal will print:
+     * WELCOME TO THE RESTAURANT
+     *
+     * 1st Customer wants: hamurger
+     *
+     * Codified version:
+     * SOP("WELCOME TO THE RESTAURANT");
+     * SOP(n + "th Customer wants: " + customer0.getOrder() );
+     ***********************************************************************************************************/
     public String getOrder(){
 	return order;
     }
 
-    //private method for setting order
-    private String setOrder(){
-    
-    }
-    */
 }
